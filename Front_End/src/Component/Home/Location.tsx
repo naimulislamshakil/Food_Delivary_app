6fbe43c7e07aef68123ae2299897bd5095f6fef9
Naimul Islam <ms0413354@gmail.com>
2022-10-11 06:49:20 +0600

    add picture

---
+++ Front_End/src/Component/Home/Location.tsx
@@ -0,0 +1,81 @@
+import dhaka from '../../assets/place/dhaka.jpg';
+import chittagong from '../../assets/place/chittagong.jpg';
+import sylhet from '../../assets/place/sylhet.jpg';
+import coxs from '../../assets/place/coxs.jpg';
+import rajshahi from '../../assets/place/rajshahi.jpg';
+import khulna from '../../assets/place/khulna.jpg';
+import comilla from '../../assets/place/comilla.jpg';
+import barisal from '../../assets/place/barisal.jpg';
+import rangpur from '../../assets/place/rangpur.jpg';
+import pirojpur from '../../assets/place/pirojpur.jpg';
+import chandpur from '../../assets/place/chandpur.jpg';
+import bhola from '../../assets/place/bhola.jpg';
+
+const CityItem = [
+	{
+		name: 'Dhaka',
+		img: dhaka,
+		letter: 'D',
+	},
+	{
+		name: 'Chittagong',
+		img: chittagong,
+		letter: 'C',
+	},
+	{
+		name: 'Sylhet',
+		img: sylhet,
+		letter: 'S',
+	},
+	{
+		name: "Cox's Bazar",
+		img: coxs,
+		letter: 'C',
+	},
+	{
+		name: 'Rajshahi',
+		img: rajshahi,
+		letter: 'R',
+	},
+	{
+		name: 'Khulna',
+		img: khulna,
+		letter: 'K',
+	},
+	{
+		name: 'Comilla',
+		img: comilla,
+		letter: 'C',
+	},
+	{
+		name: 'Barisal',
+		img: barisal,
+		letter: 'B',
+	},
+	{
+		name: 'Rangpur',
+		img: rangpur,
+		letter: 'R',
+	},
+	{
+		name: 'Pirojpur',
+		img: pirojpur,
+		letter: 'P',
+	},
+	{
+		name: 'Chandpur',
+		img: chandpur,
+		letter: 'C',
+	},
+	{
+		name: 'Bhola',
+		img: bhola,
+		letter: 'B',
+	},
+];
+
+const Location = () => {
+	return <section className="container-fluid"></section>;
+};
+
+export default Location;
