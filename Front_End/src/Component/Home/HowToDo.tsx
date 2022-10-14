2878316c3100f522e45b76c0491e67b90e8db46e
Naimul Islam <ms0413354@gmail.com>
2022-10-14 15:12:53 +0600

    add row 5

--- Front_End/src/Component/Home/HowToDo.tsx
+++ Front_End/src/Component/Home/HowToDo.tsx
@@ -5,7 +5,9 @@ import playStore from '../../assets/how/play-store.png';
 import rode from '../../assets/how/step-illustration.png';
 import select from '../../assets/how/Select-and-set-your-food.jpg';
 import order from '../../assets/how/pathao-food-delivery.jpg';
+import cash from '../../assets/how/pay-info.png';
 import '../../scss/App/App.css';
+import { Link } from 'react-router-dom';
 
 const HowToDo = () => {
 	return (
@@ -52,7 +54,7 @@ const HowToDo = () => {
 			{/* row 03 */}
 			<div className="row">
 				<div className="col-12 col-lg-5">
-					<img src={select} alt="" />
+					<img src={select} className="img-fluid" alt="" />
 				</div>
 				<div className="col-12 col-lg-7">
 					<h2 className="h1 text-color-number">02</h2>
@@ -77,7 +79,30 @@ const HowToDo = () => {
 					</div>
 				</div>
 				<div className="col-12 col-lg-5">
-					<img src={order} alt="" />
+					<img src={order} className="img-fluid" alt="" />
+				</div>
+			</div>
+			{/* row 05 */}
+			<div className="row mt-5">
+				<div className="col-12 col-lg-7 d-flex flex-column justify-content-center align-items-start">
+					<h2 className="h1 text-color-number">04</h2>
+					<div>
+						<h2>Pay Cashless for your Food</h2>
+						<h4 className="text-color">
+							Have no cash? No worries! <br /> Pay digitally instead of cash.
+						</h4>
+						<Link
+							className="nav-link text-danger"
+							style={{ fontSize: '18px' }}
+							to="/"
+						>
+							Learn More
+							<i className="bi bi-arrow-right ms-2"></i>
+						</Link>
+					</div>
+				</div>
+				<div className="col-12 col-lg-5">
+					<img src={cash} className="img-fluid" alt="" />
 				</div>
 			</div>
 		</section>
