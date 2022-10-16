7f694469461c87afaeaf1297f5b5b957d3463fc0
Naimul Islam <ms0413354@gmail.com>
2022-10-16 18:52:24 +0600

    add root reducer

--- Front_End/src/Redux/Store.ts
+++ Front_End/src/Redux/Store.ts
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import RootReducer from './Reducer/RootReducer';
 
 const Store = createStore(
 	RootReducer,
