5c7e8b9186aa05e8b762b7dcc1f188df6ee93383
Naimul Islam <ms0413354@gmail.com>
2022-10-19 20:59:33 +0600

    add a navbar

--- Front_End/src/Component/StoreDetils.tsx
+++ Front_End/src/Component/StoreDetils.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { GetSingleStoreAction } from '../Redux/Action/Action/GetByIdAction';
 import { RootStore } from '../Redux/Store';
+import '../scss/App/App.css';
 
 const StoreDetils = () => {
 	const current = new Date();
@@ -19,33 +20,55 @@ const StoreDetils = () => {
 	return (
 		<section className="container-fluid">
 			<div className="row">
-				<div className="col-12">
+				<div className="col-12 p-3">
 					<img
 						className="w-100 img-fluid h-25"
 						src={store?.data.imgUrl}
 						alt=""
 					/>
-					<div>
+					<div className="p-3">
 						<h2>{store?.data.name}</h2>
-						<span className="bg-danger text-white p-2 rounded-pill ms-3">
+						<span className="bg-danger text-white p-2 rounded-pill">
 							{store?.data.place}
 						</span>
 						{current.toLocaleTimeString() > '8:00:00' &&
 						current.toLocaleTimeString() < '16:00:00' ? (
-							<span className="bg-success text-white p-2 rounded-pill ms-3">
+							<span className="bg-success text-white p-2 rounded-pill ms-2">
 								Open
 							</span>
 						) : (
-							<span className="bg-warning text-white p-2 rounded-pill ms-3">
+							<span className="bg-warning text-white p-2 rounded-pill ms-2">
 								Close
 							</span>
 						)}
-						<div className="mt-2 ms-3">
+						<div className="mt-2">
 							<span className="text-danger">$$$</span>
-							{store?.data.tag.map((tag) => (
-								<span className="ms-2 text-danger">- {tag}</span>
+							{store?.data.tag.map((tag, index) => (
+								<span key={index} className="ms-2 text-danger">
+									- {tag}
+								</span>
 							))}
 						</div>
+					</div>
+					<div className="p-3 border-2 bg-color">
+						<h4>VOUCHER: HOMEFOOD65</h4>
+						<p>
+							Use VOUCHER: HOMEFOOD65 to get 26% off up to BDT 65 on orders over
+							BDT 249
+						</p>
+					</div>
+					<div className="bg-white p-3 shadow-lg mt-3">
+						<nav>
+							<ul className="">
+								{/* 'Rich', 'Fish', 'Vagetable', 'Biryani', 'Chicken', 'Soup' */}
+								<li className="">Rich</li>
+								<li className="">Fish</li>
+								<li className="">Vagetable</li>
+								<li className="">Biryani</li>
+								<li className="">Chicken</li>
+								<li className="">Soup</li>
+							</ul>
+						</nav>
 					</div>
 				</div>
 			</div>
