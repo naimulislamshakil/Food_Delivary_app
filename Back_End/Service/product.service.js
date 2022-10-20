b9f0e1538d42e7435c93b93c6ecc114fd4a29d73
Naimul Islam <ms0413354@gmail.com>
2022-10-20 07:04:26 +0600

    add product in store

--- Back_End/Service/product.service.js
+++ Back_End/Service/product.service.js
@@ -1,7 +1,18 @@
 const Product = require('../Model/product.model');
+const Store = require('../Model/store.model');
 
 exports.postAProductService = async (product) => {
-	return await Product.create(product);
+	const result = await Product.create(product);
+
+	const { _id, store } = result;
+	const res = await Store.Store.updateOne(
+		{ _id: store.id },
+		{
+			$push: { product: _id },
+		}
+	);
+
+	return result;
 };
 
 exports.getAllProductService = async () => {
