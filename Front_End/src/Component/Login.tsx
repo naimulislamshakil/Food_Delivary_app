1b0303d558c0163f08b4a178bfebba76ac7da6ce
Naimul Islam <ms0413354@gmail.com>
2022-10-22 03:57:15 +0600

    add login form

--- Front_End/src/Component/Login.tsx
+++ Front_End/src/Component/Login.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 	return (
 		<section className="container-fluid">
-			<h2>lioin</h2>
+			<form onSubmit={onSubmit} className="w-75 mx-auto card p-5">
+				<h2 className="text-center">Log In</h2>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">
+						Email address
+					</label>
+					<input
+						onBlur={(e) => setEmail(e.target.value)}
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+					/>
+					<div id="emailHelp" className="form-text">
+						We'll never share your email with anyone else.
+					</div>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputPassword1" className="form-label">
+						Password
+					</label>
+					<input
+						onBlur={(e) => setPassword(e.target.value)}
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+					/>
+				</div>
+				<div className="mb-3 d-lg-flex justify-content-lg-center d-block">
+					<h6>You Are Not A User!</h6>
+					<Link to="/register" className="nav-link">
+						<h6 className="ms-2 text-decoration-underline text-primary">
+							Please Register.
+						</h6>
+					</Link>
+				</div>
+				<input
+					type="submit"
+					value="Login"
+					className="btn w-lg-25 w-100 fw-bold mx-auto"
+					style={{ backgroundColor: 'turquoise', color: 'white' }}
+				/>
+			</form>
 		</section>
 	);
 };
