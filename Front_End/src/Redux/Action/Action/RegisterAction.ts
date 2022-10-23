6f2461d7008546ccb8d9af898d0ee0d9f8719537
Naimul Islam <ms0413354@gmail.com>
2022-10-23 08:17:40 +0600

    add register action

--- Front_End/src/Redux/Action/Action/RegisterAction.ts
+++ Front_End/src/Redux/Action/Action/RegisterAction.ts
@@ -3,7 +3,9 @@ import { Dispatch } from 'redux';
 import {
 	RegisterDispatchType,
 	RegisterUesr,
+	REGISTER_FAIL,
 	REGISTER_LOADING,
+	REGISTER_SUCCESS,
 } from '../ActionType/RegisterActionType';
 
 export const RegisterAction =
@@ -13,6 +15,15 @@ export const RegisterAction =
 				type: REGISTER_LOADING,
 			});
 
-			const res = await axios.post();
-		} catch (error) {}
+			const res = await axios.post('http://localhost:5000/api/v1/user', user);
+
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			});
+		} catch (error) {
+			dispatch({
+				type: REGISTER_FAIL,
+			});
+		}
 	};
