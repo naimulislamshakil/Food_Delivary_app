2f18ca8200c13e1f3fc4d2ae35c694663ac1b6da
Naimul Islam <ms0413354@gmail.com>
2022-10-27 07:44:57 +0600

    login action create

---
+++ Front_End/src/Redux/Action/Action/LoginAction.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import {
+	LoginDispatchType,
+	LOGIN_FAIL,
+	LOGIN_LOADING,
+	LOGIN_SUSSESS,
+} from '../ActionType/LoginActionType';
+
+interface Props {
+	email: string;
+	password: string;
+}
+
+export const LoginAction =
+	(user: Props) => async (dispatch: Dispatch<LoginDispatchType>) => {
+		try {
+			dispatch({
+				type: LOGIN_LOADING,
+			});
+
+			const res = await axios.post('http://localhost:5000/api/v1/login', user);
+			dispatch({
+				type: LOGIN_SUSSESS,
+				payload: res.data,
+			});
+		} catch (error) {
+			dispatch({
+				type: LOGIN_FAIL,
+			});
+		}
+	};
