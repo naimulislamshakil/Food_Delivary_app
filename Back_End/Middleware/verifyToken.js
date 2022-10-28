238dc310d200944fc7224c139d5d00d1a177fb64
Naimul Islam <ms0413354@gmail.com>
2022-10-28 23:09:25 +0600

    complete verify token

--- Back_End/Middleware/verifyToken.js
+++ Back_End/Middleware/verifyToken.js
@@ -1,13 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
 module.exports = async (req, res, next) => {
 	try {
 		const token = req.headers?.authorization?.split(' ')[1];
 
 		if (!token) {
-			res.status(401).json({
+			return res.status(401).json({
 				status: 'Faild',
 				message: 'Pleace login again.',
 			});
 		}
+
+		const decoded = await promisify(jwt.verify)(
+			token,
+			process.env.TOKEN_SECRET
+		);
+
+		req.user = decoded;
+		next();
 	} catch (error) {
 		res.status(500).json({
 			status: 'Faild',
