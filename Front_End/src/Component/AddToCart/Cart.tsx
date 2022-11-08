ad19bb44f1ab04ecba45d95641e26862dbaf6962
Naimul Islam <ms0413354@gmail.com>
2022-11-08 22:07:09 +0600

    start work add to cart

--- Front_End/src/Component/AddToCart/Cart.tsx
+++ Front_End/src/Component/AddToCart/Cart.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../scss/App/App.css';
 
 const Cart = () => {
 	return (
@@ -9,9 +10,299 @@ const Cart = () => {
 			</p>
 
 			<div className="row">
-				<div className="col-12 rounded-3" style={{ background: '#f5e2df' }}>
+				<div className="col-12 rounded-3">
 					{/* product information */}
-					<div className="cart_main_container">
+					<div
+						className="cart_main_container"
+						style={{ background: '#f5e2df' }}
+					>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
+						<div className="item-info p-2 d-flex align-items-center justify-content-center">
+							<div className="product-img">
+								<img
+									src="https://i.ibb.co/CbmfpLt/image.jpg"
+									alt=""
+									style={{
+										width: '120px',
+										height: '80px',
+										borderRadius: '10px',
+									}}
+								/>
+							</div>
+
+							<div className="title ms-5">
+								<h6>Perfect Long-Grain White Rice</h6>
+								<p>Rich</p>
+							</div>
+
+							<div className="input-field ms-5">
+								<button className="btn btn-outline-success">
+									<i className="bi bi-plus"></i>
+								</button>
+								<input
+									className="ms-2 text-center w-25"
+									disabled
+									type="text"
+									placeholder="2"
+								/>
+								<button className="btn btn-outline-danger ms-2">
+									<i className="bi bi-dash"></i>
+								</button>
+							</div>
+
+							<div className="price ms-5">
+								<h6>40৳</h6>
+							</div>
+
+							<div className="remove ms-5">
+								<button className="btn btn-outline-danger">X</button>
+							</div>
+						</div>
 						<div className="item-info p-2 d-flex align-items-center justify-content-center">
 							<div className="product-img">
 								<img
