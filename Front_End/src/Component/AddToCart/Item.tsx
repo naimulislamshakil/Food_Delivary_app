0ddfa622ac4da29a6e81130877a0351c6b717aa9
Naimul Islam <ms0413354@gmail.com>
2022-11-08 22:55:30 +0600

    add plus function

--- Front_End/src/Component/AddToCart/Item.tsx
+++ Front_End/src/Component/AddToCart/Item.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Item = () => {
+	const [value, setValue] = useState('1');
+
+	const plus = () => {
+		const numValue = parseInt(value) + 1;
+		const string = numValue.toString();
+		setValue(string);
+	};
 	return (
-		<div>
-			<h2>Item</h2>
+		<div className="item-info p-2 d-flex align-items-center justify-content-center">
+			<div className="product-img">
+				<img
+					src="https://i.ibb.co/CbmfpLt/image.jpg"
+					alt=""
+					style={{
+						width: '120px',
+						height: '80px',
+						borderRadius: '10px',
+					}}
+				/>
+			</div>
+
+			<div className="title ms-5">
+				<h6>Perfect Long-Grain White Rice</h6>
+				<p>Rich</p>
+			</div>
+
+			<div className="input-field ms-5">
+				<button className="btn btn-outline-success" onClick={(e) => plus()}>
+					<i className="bi bi-plus"></i>
+				</button>
+				<input
+					className="ms-2 text-center w-25"
+					disabled
+					type="text"
+					placeholder={value}
+				/>
+				<button className="btn btn-outline-danger ms-2">
+					<i className="bi bi-dash"></i>
+				</button>
+			</div>
+
+			<div className="price ms-5">
+				<h6>40৳</h6>
+			</div>
+
+			<div className="remove ms-5">
+				<button className="btn btn-outline-danger">X</button>
+			</div>
 		</div>
 	);
 };
