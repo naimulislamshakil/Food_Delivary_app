06a22e47cd514439ec9ad5882fbc49467030df97
Naimul Islam <ms0413354@gmail.com>
2022-11-09 19:18:18 +0600

    complete addtocart action type

--- Front_End/src/Redux/Action/ActionType/AddToCartActionType.ts
+++ Front_End/src/Redux/Action/ActionType/AddToCartActionType.ts
@@ -1,1 +1,25 @@
-const;
+export const ADDTOCART_LOADING = 'ADDTOCART_LOADING';
+export const ADDTOCART_FAIL = 'ADDTOCART_FAIL';
+export const ADDTOCART_SUCCESS = 'ADDTOCART_SUCCESS';
+
+export type AddToCart = {
+	status: string;
+	message: string;
+};
+
+export interface AddToCartLoading {
+	type: typeof ADDTOCART_LOADING;
+}
+export interface AddToCartFail {
+	type: typeof ADDTOCART_FAIL;
+	payload: AddToCart;
+}
+export interface AddToCartSuccess {
+	type: typeof ADDTOCART_SUCCESS;
+	payload: AddToCart;
+}
+
+export type AddToCartDispatchType =
+	| AddToCartFail
+	| AddToCartLoading
+	| AddToCartSuccess;
