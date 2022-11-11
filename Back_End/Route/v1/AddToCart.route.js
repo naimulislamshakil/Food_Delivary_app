faca7f38c44f4619b0786d80e2efb9fa16daa6b6
Naimul Islam <ms0413354@gmail.com>
2022-11-11 21:20:50 +0600

    add a route find all add to cart base email

--- Back_End/Route/v1/AddToCart.route.js
+++ Back_End/Route/v1/AddToCart.route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const collaction = require('../../Collaction/addToCat.collaction');
 
 router.route('/addToCart').post(collaction.addToCartCreateCollaction);
+router.route('/addToCart/:email').get();
 
 module.exports = router;
