a1e62cfbc53f5c9e635be53e1cc5a9f11e4221b3
Naimul Islam <ms0413354@gmail.com>
2022-11-11 21:52:32 +0600

    create get add tocart action file

---
+++ Front_End/src/Redux/Action/Action/getAddToCartAction.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import {
+	GetAddToCartDispatchType,
+	GET_ADDTOCART_FAIL,
+	GET_ADDTOCART_LOADING,
+	GET_ADDTOCART_SUCCESS,
+} from '../ActionType/getAddToCartActionType';
+
+export const AddToCartAction =
+	(email: string) => async (dispatch: Dispatch<GetAddToCartDispatchType>) => {
+		try {
+			dispatch({
+				type: GET_ADDTOCART_LOADING,
+			});
+
+			const res = await axios.get(
+				`http://localhost:5000/api/v1/addToCart/${email}`
+			);
+
+			dispatch({
+				type: GET_ADDTOCART_SUCCESS,
+				payload: res.data,
+			});
+		} catch (error: any) {
+			dispatch({
+				type: GET_ADDTOCART_FAIL,
+				payload: error.message,
+			});
+		}
+	};
