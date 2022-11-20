a729155fb41d117bd54cc8181de50c727526913d
Naimul Islam <ms0413354@gmail.com>
2022-11-20 06:56:34 +0600

    create decressAddToCart action

---
+++ Front_End/src/Redux/Action/Action/desressAddToCartAction.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import {
+	DecressAddToCartTypeDispatch,
+	DECRESS_ADDTOCART_FAIL,
+	DECRESS_ADDTOCART_LOADING,
+	DECRESS_ADDTOCART_SUCCESS,
+} from '../ActionType/decressAddToCartActionType';
+
+export const DecressAddToCartAction =
+	(id: string) => async (dispatch: Dispatch<DecressAddToCartTypeDispatch>) => {
+		try {
+			dispatch({
+				type: DECRESS_ADDTOCART_LOADING,
+			});
+
+			const res = await axios.get(
+				`http://localhost:5000/api/v1//addToCart/decress/${id}`
+			);
+
+			dispatch({
+				type: DECRESS_ADDTOCART_SUCCESS,
+				payload: res.data,
+			});
+		} catch (error: any) {
+			dispatch({
+				type: DECRESS_ADDTOCART_FAIL,
+				payload: error.message,
+			});
+		}
+	};
